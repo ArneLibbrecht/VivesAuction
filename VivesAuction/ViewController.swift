07e4ba09565196ffdaf6e1f22cb072cc4c5b75bf
Arne Libbrecht <arnelibbrecht@Mac-van-Arne.local>
2018-11-28 01:35:19 -0800

    Initial Commit

---
+++ VivesAuction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VivesAuction
+//
+//  Created by Arne Libbrecht on 28/11/2018.
+//  Copyright © 2018 Arne Libbrecht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
